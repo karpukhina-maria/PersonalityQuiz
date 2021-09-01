34a9ef60d4e5627d190399b05c57b7dc35da1c84
Maria Karpukhina <myshka_n@mail.ru>
2021-09-02 02:00:13 +0300

    Done

--- PersonalityQuiz/ViewControllers/ResultViewController.swift
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -9,31 +9,31 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var definitionLabel: UILabel!
+    
     var answersChosen: [Answer]!
-    var result: Animal?
-
-    // 1. Передать сюда массив с ответами
-    // 2. Определить наиболее часто встречающийся тип животного
-    // 3. Отобразить результат в соответствии с этим животным
-    // 4. Избавиться от кнопки возврата на предыдущий экран
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
- 
+        navigationItem.setHidesBackButton(true, animated:true)
+        
+        guard let result = getResult() else { return }
+        resultLabel.text = "Вы - \(result.rawValue)"
+        definitionLabel.text = result.definition
     }
-        
-//        func getResult() -> Animal? {
-//            let dictionaryWithResults: [Animal : Int]  = [.cat : answersChosen.filter { $0.animal == .cat }.count,
-//                                                          .dog : answersChosen.filter { $0.animal == .dog }.count,
-//                                                          .turtle : answersChosen.filter { $0.animal == .turtle }.count,
-//                                                          .rabbit : answersChosen.filter { $0.animal == .rabbit }.count]
-//
-//            let dictionaryWithResultsSorted = dictionaryWithResults.sorted(by: {$0.value > $1.value})
-//            if let first = dictionaryWithResultsSorted.first  {
-//                result = first.key
-//            }
-//            return result
-//        }
-        
     
+    private func getResult() -> Animal? {
+        var result: Animal? = nil
+        let dictionaryWithResults: [Animal : Int]  = [.cat : answersChosen.filter { $0.animal == .cat }.count,
+                                                      .dog : answersChosen.filter { $0.animal == .dog }.count,
+                                                      .turtle : answersChosen.filter { $0.animal == .turtle }.count,
+                                                      .rabbit : answersChosen.filter { $0.animal == .rabbit }.count]
+        
+        let dictionaryWithResultsSorted = dictionaryWithResults.sorted(by: {$0.value > $1.value})
+        if let first = dictionaryWithResultsSorted.first  {
+            result = first.key
+        }
+        return result
+    }
 }
